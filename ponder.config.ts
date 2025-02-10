665c61098d030957d7a6b44a60ac17edc082a811
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2025-02-10 00:16:40 -0600

    chore: add base to ponder.config.ts

--- ponder.config.ts
+++ ponder.config.ts
@@ -6,13 +6,21 @@ import { morphoBlueAbi } from "./abis/MorphoBlue";
 export default createConfig({
   networks: {
     mainnet: { chainId: 1, transport: http(process.env.PONDER_RPC_URL_1) },
+    base: { chainId: 8453, transport: http(process.env.PONDER_RPC_URL_8453) },
   },
   contracts: {
     Morpho: {
       abi: morphoBlueAbi,
-      address: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
-      network: "mainnet",
-      startBlock: 18883124,
+      network: {
+        mainnet: {
+          address: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
+          startBlock: 18883124,
+        },
+        base: {
+          address: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
+          startBlock: 13977148,
+        },
+      },
     },
   },
 });
