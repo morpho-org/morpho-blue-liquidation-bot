a72829d2d807e9a94aa30115aef433f84f4fa91b
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-03-07 10:10:56 +0100

    feat: add market state and positions endpoints

--- apps/ponder/src/api/index.ts
+++ apps/ponder/src/api/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { and, client, eq, graphql } from "ponder";
 import { db } from "ponder:api";
 import schema from "ponder:schema";
-import { Address } from "viem";
+import type { Address } from "viem";
 
 const app = new Hono();
 
@@ -22,6 +22,36 @@ app.get("/chain/:id/vault/:address", async (c) => {
     .limit(1);
 
   return c.json(vault[0]?.withdrawQueue);
+});
+
+app.get("/chain/:id/market/:id/state", async (c) => {
+  const { id: chainId, id: marketId } = c.req.param();
+
+  const market = await db
+    .select()
+    .from(schema.market)
+    .where(
+      and(eq(schema.market.chainId, Number(chainId)), eq(schema.market.id, marketId as Address)),
+    )
+    .limit(1);
+
+  return c.json(market[0]);
+});
+
+app.get("/chain/:id/market/:id/positions", async (c) => {
+  const { id: chainId, id: marketId } = c.req.param();
+
+  const positions = await db
+    .select()
+    .from(schema.position)
+    .where(
+      and(
+        eq(schema.position.chainId, Number(chainId)),
+        eq(schema.position.marketId, marketId as Address),
+      ),
+    );
+
+  return c.json(positions);
 });
 
 export default app;
