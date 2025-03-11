ee20da84f245bff5bc479eaa604cfb85254b5f57
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2025-03-11 11:10:10 -0500

    feat: example api endpoint for vault->markets (#5)

--- apps/ponder/src/api/index.ts
+++ apps/ponder/src/api/index.ts
@@ -1,12 +1,27 @@
 import { Hono } from "hono";
-import { client, graphql } from "ponder";
+import { and, client, eq, graphql } from "ponder";
 import { db } from "ponder:api";
 import schema from "ponder:schema";
+import { Address } from "viem";
 
 const app = new Hono();
 
 app.use("/", graphql({ db, schema }));
 app.use("/graphql", graphql({ db, schema }));
 app.use("/sql/*", client({ db, schema }));
+
+app.get("/chain/:id/vault/:address", async (c) => {
+  const { id: chainId, address } = c.req.param();
+
+  const vault = await db
+    .select()
+    .from(schema.vault)
+    .where(
+      and(eq(schema.vault.chainId, Number(chainId)), eq(schema.vault.address, address as Address)),
+    )
+    .limit(1);
+
+  return c.json(vault[0]?.withdrawQueue);
+});
 
 export default app;
