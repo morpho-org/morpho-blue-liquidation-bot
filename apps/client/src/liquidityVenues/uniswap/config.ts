507ab46ad7be233223f6bca448721eec3978c7d0
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-03-12 11:35:50 +0100

    feat: add UniV3 config

--- apps/client/src/liquidityVenues/uniswap/config.ts
+++ apps/client/src/liquidityVenues/uniswap/config.ts
@@ -1,1 +1,16 @@
+import type { Address } from "viem";
+
+export const UNISWAP_ADDRESSES: Record<
+  number,
+  {
+    factory: Address;
+    router: Address;
+  }
+> = {
+  1: {
+    factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+    router: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+  },
+};
+
 export const FEE_TIERS = [500, 3000, 10000];
