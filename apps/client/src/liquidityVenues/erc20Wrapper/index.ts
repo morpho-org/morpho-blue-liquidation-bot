bcd58d3b9891bd85bccf21b22c77031f23338c41
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-03-18 15:57:45 -0300

    feat: erc20Wrapper liquidity venue

---
+++ apps/client/src/liquidityVenues/erc20Wrapper/index.ts
@@ -0,0 +1,34 @@
+import type { ExecutorEncoder } from "executooor-viem";
+import { zeroAddress, type Address } from "viem";
+
+import type { ToConvert } from "../../utils";
+import type { LiquidityVenue } from "../liquidityVenue";
+
+import { wrappers } from "./config";
+
+export class Erc20Wrapper implements LiquidityVenue {
+  private underlying: Address = zeroAddress;
+
+  supportsRoute(encoder: ExecutorEncoder, src: Address, dst: Address) {
+    if (src === dst) return false;
+    const underlying = this.getUnderlying(src, encoder.client.chain.id);
+    if (underlying !== undefined) {
+      this.underlying = underlying;
+      return true;
+    }
+
+    return false;
+  }
+
+  convert(encoder: ExecutorEncoder, toConvert: ToConvert) {
+    const { src, dst, srcAmount } = toConvert;
+
+    encoder.erc20WrapperWithdrawTo(src, encoder.address, srcAmount);
+
+    return { src: this.underlying, dst, srcAmount };
+  }
+
+  private getUnderlying(src: Address, chainId: number) {
+    return wrappers[chainId]?.[src];
+  }
+}
