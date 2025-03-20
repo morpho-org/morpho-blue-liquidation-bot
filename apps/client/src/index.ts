c5c22e38bbebbe039563dd877db2e3e87122b19e
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-03-20 08:38:00 -0300

    feat: abstract liquidity venues

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -7,34 +7,27 @@ import { ExecutorEncoder } from "executooor-viem";
 import { chainConfigs } from "../config.js";
 import { fetchLiquidatablePositions, fetchWhiteListedMarketsForVault } from "./utils/fetchers.js";
 
-import { Erc4626 } from "./liquidityVenues/erc4626/index.js";
-import { Erc20Wrapper } from "./liquidityVenues/erc20Wrapper/index.js";
-import { UniswapV3 } from "./liquidityVenues/uniswap/index.js";
-
 import { morphoBlueAbi } from "../../ponder/abis/MorphoBlue";
 import type { ChainConfig } from "./utils/types.js";
+import type { LiquidityVenue } from "./liquidityVenues/liquidityVenue.js";
 
 class LiquidationBot {
   private chainId: number;
   private chainConfig: ChainConfig;
 
-  private erc20Wrapper: Erc20Wrapper;
-  private erc4626: Erc4626;
-  private uniswapV3: UniswapV3;
+  private liquidationVenues: LiquidityVenue[];
 
   /// TODO: import address from config. I think their should be only one config file for both apps
   MORPHO_ADDRESS = "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb" as Address;
 
-  constructor(chainId: number) {
+  constructor(chainId: number, liquidationVenues: LiquidityVenue[]) {
     if (chainConfigs[chainId] === undefined) {
       throw new Error(`Chain ${chainId} not supported`);
     }
 
     this.chainId = chainId;
     this.chainConfig = chainConfigs[chainId];
-    this.erc20Wrapper = new Erc20Wrapper();
-    this.erc4626 = new Erc4626();
-    this.uniswapV3 = new UniswapV3();
+    this.liquidationVenues = liquidationVenues;
   }
 
   async run() {
@@ -73,17 +66,10 @@ class LiquidationBot {
 
         /// LIQUIDITY VENUES
 
-        /// Erc20Wrapper
-        if (this.erc20Wrapper.supportsRoute(encoder, toConvert.src, toConvert.dst))
-          toConvert = this.erc20Wrapper.convert(encoder, toConvert);
-
-        /// Erc4626
-        if (await this.erc4626.supportsRoute(encoder, toConvert.src, toConvert.dst))
-          toConvert = await this.erc4626.convert(encoder, toConvert);
-
-        /// UniswapV3
-        if (await this.uniswapV3.supportsRoute(encoder, toConvert.src, toConvert.dst))
-          toConvert = await this.uniswapV3.convert(encoder, toConvert);
+        for (const venue of this.liquidationVenues) {
+          if (await venue.supportsRoute(encoder, toConvert.src, toConvert.dst))
+            toConvert = await venue.convert(encoder, toConvert);
+        }
 
         const callbacks = encoder.flush();
 
