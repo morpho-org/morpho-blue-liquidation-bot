007eb221f8710dd9d41092df4b4cbffdcac5e237
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-03-25 15:45:55 -0300

    feat: finalize bot

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -1,106 +1,54 @@
-import { type Address, createWalletClient, http } from "viem";
-import { simulateCalls } from "viem/actions";
+import { createWalletClient, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
+import { watchBlocks } from "viem/actions";
 
-import { ExecutorEncoder } from "executooor-viem";
+import { chainConfigs } from "../config";
+import { LiquidationBot } from "./bot";
 
-import { chainConfigs } from "../config.js";
-import { fetchLiquidatablePositions, fetchWhiteListedMarketsForVault } from "./utils/fetchers.js";
+import { UniswapV3 } from "./liquidityVenues/uniswap";
+import { Erc20Wrapper } from "./liquidityVenues/erc20Wrapper";
+import { Erc4626 } from "./liquidityVenues/erc4626";
 
-import type { ChainConfig } from "./utils/types.js";
-import type { LiquidityVenue } from "./liquidityVenues/liquidityVenue.js";
-
-export class LiquidationBot {
-  private chainId: number;
-  private chainConfig: ChainConfig;
-
-  private liquidationVenues: LiquidityVenue[];
+const main = () => {
+  const args = process.argv.slice(2);
+  const chainIdArg = args.find((arg) => arg.startsWith("--chainId="));
 
   /// TODO: import address from config. I think their should be only one config file for both apps
-  MORPHO_ADDRESS = "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb" as Address;
+  const MORPHO_ADDRESS = "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb";
 
-  constructor(chainId: number, liquidationVenues: LiquidityVenue[]) {
-    if (chainConfigs[chainId] === undefined) {
-      throw new Error(`Chain ${chainId} not supported`);
-    }
+  if (chainIdArg === undefined) {
+    throw new Error("Chain ID is missing");
+  }
+  const chainId = Number(chainIdArg);
 
-    this.chainId = chainId;
-    this.chainConfig = chainConfigs[chainId];
-    this.liquidationVenues = liquidationVenues;
+  if (chainConfigs[chainId] === undefined) {
+    throw new Error(`Chain ${chainId} not supported`);
   }
 
-  async run() {
-    const { vaultWhitelist } = this.chainConfig;
-    const whitelistedMarkets = [
-      ...new Set(
-        (
-          await Promise.all(
-            vaultWhitelist.map((vault) => fetchWhiteListedMarketsForVault(this.chainId, vault)),
-          )
-        ).flat(),
-      ),
-    ];
+  const client = createWalletClient({
+    chain: chainConfigs[chainId].chain,
+    transport: http(chainConfigs[chainId].rpcUrl),
+    account: privateKeyToAccount(chainConfigs[chainId].liquidationPrivateKey),
+  });
 
-    const liquidatablePositions = await fetchLiquidatablePositions(
-      this.chainId,
-      whitelistedMarkets,
-    );
+  // LIQUIDITY VENUES
+  const liquidityVenues = [];
+  liquidityVenues.push(new UniswapV3());
+  liquidityVenues.push(new Erc20Wrapper());
+  liquidityVenues.push(new Erc4626());
 
-    const client = createWalletClient({
-      chain: this.chainConfig.chain,
-      transport: http(this.chainConfig.rpcUrl),
-      account: privateKeyToAccount(this.chainConfig.liquidationPrivateKey),
-    });
-    const executorAddress = this.chainConfig.executorAddress;
+  const bot = new LiquidationBot(
+    chainId,
+    client,
+    MORPHO_ADDRESS,
+    chainConfigs[chainId].vaultWhitelist,
+    chainConfigs[chainId].executorAddress,
+    liquidityVenues,
+  );
 
-    await Promise.all(
-      liquidatablePositions.map(async (liquidatablePosition) => {
-        let toConvert = {
-          src: liquidatablePosition.marketParams.loanToken,
-          dst: liquidatablePosition.marketParams.collateralToken,
-          srcAmount: liquidatablePosition.seizableCollateral,
-        };
-
-        const encoder = new ExecutorEncoder(executorAddress, client);
-
-        /// LIQUIDITY VENUES
-
-        for (const venue of this.liquidationVenues) {
-          if (await venue.supportsRoute(encoder, toConvert.src, toConvert.dst))
-            toConvert = await venue.convert(encoder, toConvert);
-        }
-
-        encoder.morphoBlueLiquidate(
-          this.MORPHO_ADDRESS,
-          liquidatablePosition.marketParams,
-          liquidatablePosition.position.user,
-          liquidatablePosition.seizableCollateral,
-          0n,
-          encoder.flush(),
-        );
-
-        const liquidationCall = encoder.flush()[0];
-
-        /// TX SIMULATION
-
-        const { results } = await simulateCalls(encoder.client, {
-          calls: [
-            {
-              to: encoder.address,
-              data: liquidationCall,
-            },
-          ],
-        });
-
-        // TX EXECUTION
-
-        if (results[0].status === "success") {
-          await client.sendTransaction({
-            to: encoder.address,
-            data: liquidationCall,
-          });
-        }
-      }),
-    );
-  }
-}
+  watchBlocks(client, {
+    onBlock: () => {
+      void bot.run();
+    },
+  });
+};
