9c87e6b9d4deb802d76e3e6c83f977d47c99f969
Hayden Shively <hayden@morpho.xyz>
2025-05-30 01:27:00 -0500

    revert accidental change

--- apps/config/src/liquidityVenues/uniswapV3.ts
+++ apps/config/src/liquidityVenues/uniswapV3.ts
@@ -1,5 +1,5 @@
 import type { Address } from "viem";
-import { arbitrum, base } from "viem/chains";
+import { base } from "viem/chains";
 
 export const MIN_SQRT_RATIO = 4295128739n;
 export const MAX_SQRT_RATIO = 1461446703485210103287273052203988822378723970342n;
@@ -8,7 +8,6 @@ export const DEFAULT_FACTORY_ADDRESS = "0x1F98431c8aD98523631AE4a59f267346ea31F9
 
 export const specificFactoryAddresses: Record<number, Address> = {
   [base.id]: "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",
-  [arbitrum.id]: "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",
 };
 
 export const FEE_TIERS = [500, 3000, 10000];
