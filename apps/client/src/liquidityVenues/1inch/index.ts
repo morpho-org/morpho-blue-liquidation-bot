767f688cbb28687b57883f762d23b9abaef9c94d
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-09-30 10:41:20 +0200

    feat: add 1inch liquidity venue

---
+++ apps/client/src/liquidityVenues/1inch/index.ts
@@ -0,0 +1,130 @@
+import { API_BASE_URL, slippage, supportedNetworks } from "@morpho-blue-liquidation-bot/config";
+import { ExecutorEncoder } from "executooor-viem";
+import { LiquidityVenue } from "../liquidityVenue";
+import { Address, Hex } from "viem";
+import { ToConvert } from "../../utils/types";
+import { BigIntish } from "@morpho-org/blue-sdk";
+
+interface SwapParams {
+  chainId: BigIntish;
+  src: string;
+  dst: string;
+  amount: BigIntish;
+  from: string;
+  origin: string;
+  slippage: BigIntish;
+  protocols?: string;
+  fee?: BigIntish;
+  gasPrice?: BigIntish;
+  complexityLevel?: number;
+  parts?: number;
+  mainRouteParts?: number;
+  gasLimit?: BigIntish;
+  includeTokensInfo?: boolean;
+  includeProtocols?: boolean;
+  includeGas?: boolean;
+  connectorTokens?: string;
+  excludedProtocols?: string;
+  permit?: string;
+  receiver?: string;
+  referrer?: string;
+  allowPartialFill?: boolean;
+  disableEstimate?: boolean;
+  usePermit2?: boolean;
+}
+
+interface SwapToken {
+  symbol: string;
+  name: string;
+  decimals: number;
+  address: string;
+  logoURI: string;
+}
+
+interface SwapResponse {
+  srcToken: SwapToken;
+  dstToken: SwapToken;
+  dstAmount: string;
+  protocols: {}[];
+  tx: {
+    to: Address;
+    data: Hex;
+    value: bigint;
+  };
+}
+
+export class OneInch implements LiquidityVenue {
+  private apiKey: string | undefined;
+
+  constructor() {
+    this.apiKey = process.env.ONE_INCH_SWAP_API_KEY;
+  }
+
+  supportsRoute(encoder: ExecutorEncoder, src: Address, dst: Address) {
+    if (!supportedNetworks.includes(encoder.client.chain.id)) return false;
+    return this.apiKey !== undefined;
+  }
+
+  async convert(encoder: ExecutorEncoder, toConvert: ToConvert) {
+    try {
+      const swapResponse = await this.fetchSwap({
+        chainId: encoder.client.chain.id,
+        src: toConvert.src,
+        dst: toConvert.dst,
+        amount: toConvert.srcAmount,
+        from: encoder.address,
+        slippage,
+        origin: encoder.client.account.address,
+        includeTokensInfo: false,
+        includeProtocols: false,
+        includeGas: false,
+        allowPartialFill: false,
+        disableEstimate: true,
+        usePermit2: false,
+      });
+
+      encoder
+        .erc20Approve(toConvert.src, swapResponse.tx.to, toConvert.srcAmount)
+        .pushCall(swapResponse.tx.to, BigInt(swapResponse.tx.value), swapResponse.tx.data);
+
+      /// assumed to be the last liquidity venue
+      return {
+        src: toConvert.dst,
+        dst: toConvert.dst,
+        srcAmount: 0n,
+      };
+    } catch (error) {
+      console.log(`Error swapping ${toConvert.src} to ${toConvert.dst} on 1inch`);
+      console.error(error);
+      return toConvert;
+    }
+  }
+
+  private getSwapApiPath = (chainId: BigIntish) => `/swap/v6.0/${chainId}/swap`;
+
+  private async fetchSwap(swapParams: SwapParams) {
+    const url = new URL(this.getSwapApiPath(swapParams.chainId), API_BASE_URL);
+    Object.entries(swapParams).forEach(([key, value]) => {
+      if (value == null) return;
+      switch (key) {
+        case "slippage":
+          // 1inch expects slippage as a percentage, so we divide our value (in basis points) by 100
+          url.searchParams.set(key, (Number(value) / 100).toString(10));
+          break;
+        default:
+          url.searchParams.set(key, value);
+      }
+    });
+
+    const res = await fetch(url, {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+    });
+
+    if (!res.ok) throw Error(res.statusText);
+
+    return (await res.json()) as SwapResponse;
+  }
+}
