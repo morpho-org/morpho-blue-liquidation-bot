95084ec72883b28760635eb97aaf4bdb4674816f
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-10-27 16:52:41 +0100

    fix: uniswapV3 config

--- apps/config/src/pricers/uniswapV3.ts
+++ apps/config/src/pricers/uniswapV3.ts
@@ -1,10 +1,12 @@
 import type { Address } from "viem";
-import { base, mainnet, katana, arbitrum, unichain } from "viem/chains";
+import { arbitrum, base, mainnet, polygon, unichain, worldchain, katana } from "viem/chains";
 
 export const USD_REFERENCE: Record<number, Address> = {
   [mainnet.id]: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
   [base.id]: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
   [katana.id]: "0x203A662b0BD271A6ed5a60EdFbd04bFce608FD36",
-  [arbitrum.id]: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
-  [unichain.id]: "0x078D782b760474a361dDA0AF3839290b0EF57AD6",
+  [arbitrum.id]: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  [unichain.id]: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  [polygon.id]: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  [worldchain.id]: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
 };
