4ac664d24c085573a7c2bea5e7cbb7000d689a74
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-11-26 14:41:57 +0100

    fix: adapt sentry config

--- apps/client/src/script.ts
+++ apps/client/src/script.ts
@@ -11,7 +11,8 @@ import { launchBot } from ".";
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   environment: process.env.NODE_ENV || "development",
-  tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1.0,
+  sampleRate: 1,
+  tracesSampleRate: 0,
   debug: process.env.NODE_ENV === "development",
   integrations: [
     // Automatically instrument Node.js libraries and frameworks
